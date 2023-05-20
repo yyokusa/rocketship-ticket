16e079818e1de31928a526b89385aee022176236
Yasin A. Yokus <ayokus@icloud.com>
2023-05-20 21:20:06 +0300

    test: add single parameter filtering tests

--- test/sensors/users.test.ts
+++ test/sensors/users.test.ts
@@ -1,28 +1,26 @@
 import app from '../../app';
 import supertest from 'supertest';
 import { expect } from 'chai';
-import shortid from 'shortid';
 import mongoose from 'mongoose';
 
 const roomA = "Room A";
+const roomB = "Room B";
+const roomC = "Room C";
 const measurementTemperature = "Temperature";
+const measurementHumidity = "Humidity";
 const datetime = "2022-08-23T23:05:12Z";
+const startTime = "2022-08-26T16:33:55Z";
+const endTime = "2022-08-26T19:25:30Z";
 const value = 22.8;
 
 const sensorData = {
     Value: 22.8,
-    Datetime: "2022-08-23T23:05:12Z",
+    Datetime: "2022-08-23T22:06:12Z",
     Room: "Room C",
     Measurement: "Temperature"
 };
 
 const sensorDataBulk = [
-    {
-        Value: 22.8,
-        Datetime: "2022-08-23T22:06:12Z",
-        Room: "Room C",
-        Measurement: "Temperature"
-    },
     {
         Value: 18.5,
         Datetime: "2022-08-23T14:11:25Z",
@@ -75,13 +73,13 @@ const sensorDataBulk = [
         Value: 20.9,
         Datetime: "2022-08-29T11:19:00Z",
         Room: "Room C",
-        Measurement: "Humidity"
+        Measurement: "Temperature"
     }
 ];
 
 describe('sensors endpoints', function () {
     let request: supertest.SuperAgentTest;
-    before(function () {
+    before(async function () {
         request = supertest.agent(app);
     });
 
@@ -113,14 +111,103 @@ describe('sensors endpoints', function () {
         expect(res.body.message).to.be.equal('success');
     });
 
-    it('should allow a GET from /sensors with room filter', async function () {
+    it('should allow a GET from /sensors with startTime filter', async function () {
+        const res = await request
+            .get(`/sensors?startTime=${startTime}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+    
+    it('should allow a GET from /sensors with endTime filter', async function () {
+        const res = await request
+            .get(`/sensors?endTime=${endTime}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+    
+    it('should allow a GET from /sensors with room A filter', async function () {
         const res = await request
             .get(`/sensors?room=${roomA}`)
             .send();
         expect(res.status).to.equal(200);
         expect(res.body).not.to.be.empty;
         expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(3);
         expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
     });
+    
+    it('should allow a GET from /sensors with room B filter', async function () {
+        const res = await request
+            .get(`/sensors?room=${roomB}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(3);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+    
+    it('should allow a GET from /sensors with room C filter', async function () {
+        const res = await request
+            .get(`/sensors?room=${roomC}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(4);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+    
+    it('should allow a GET from /sensors with measurement temperature filter', async function () {
+        const res = await request
+            .get(`/sensors?measurement=${measurementTemperature}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(6);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+    
+    it('should allow a GET from /sensors with measurement humidity filter', async function () {
+        const res = await request
+            .get(`/sensors?measurement=${measurementHumidity}`)
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty;
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(4);
+        expect(res.body[0]).to.have.property('_id');
+        expect(res.body[0]).to.have.property('Value');
+        expect(res.body[0]).to.have.property('Datetime');
+        expect(res.body[0]).to.have.property('Room');
+    });
+
 });
 
